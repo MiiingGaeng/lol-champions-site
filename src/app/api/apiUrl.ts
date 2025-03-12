f83372c04e8e819613d1e9d60df7579907ad7169
miing_gaeng <pem4134@naver.com>
2025-03-12 17:33:01 +0900

    feat: api 통신을 위한 URL #3

---
+++ src/app/api/apiUrl.ts
@@ -0,0 +1,6 @@
+import { LANGUAGE } from "@/constants/language";
+
+export const dataApiUrl: string = `https://ddragon.leagueoflegends.com/cdn/15.5.1/data/${LANGUAGE.KO}`;
+
+export const imgApiUrl: string =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img";
---
+++ src/constants/language.ts
@@ -0,0 +1,9 @@
+type Language = {
+  KO: string;
+  EN: string;
+};
+
+export const LANGUAGE: Language = {
+  KO: "ko_KR",
+  EN: "en_US"
+};
