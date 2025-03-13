ac19d5187d768a610070fc042a8a2e5bd3629ab7
miing_gaeng <pem4134@naver.com>
2025-03-13 17:22:40 +0900

    feat: Champion 기본데이터 타입 지정 #5

---
+++ src/types/Champions.ts
@@ -0,0 +1,38 @@
+export type Champion = {
+  id: string;
+  name: string;
+  title: string;
+  info: {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+  };
+  image: {
+    full: string;
+  };
+  tags: string[];
+  partype: string;
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  };
+};
