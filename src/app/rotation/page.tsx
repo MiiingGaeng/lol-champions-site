d61a11340a0e58b4f0ce4db4ff228599fad35f42
miing_gaeng <pem4134@naver.com>
2025-03-14 19:18:24 +0900

    feat: 로테이션 페이지 구현 #7

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,5 +1,13 @@
+import RotationListForAll from "@/components/_/RotationListForAll";
+import RotationListForNew from "@/components/_/RotationListForNew";
+
 const rotationPage = () => {
-  return <div>rotationPage</div>;
+  return (
+    <div>
+      <RotationListForAll />
+      <RotationListForNew />
+    </div>
+  );
 };
 
 export default rotationPage;
