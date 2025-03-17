c316ca87a1c6e4fbdbe63f0c011de08ac0e130c1
miing_gaeng <pem4134@naver.com>
2025-03-17 16:59:27 +0900

    refactor: 아이템, 챔피언 type 수정 #9

--- src/types/Champions.ts
+++ src/types/Champions.ts
@@ -1,31 +1,147 @@
 export type Champion = {
+  version: string;
   id: string;
+  key: string;
   name: string;
   title: string;
-  image: {
-    full: string;
-  };
-  tags: string[];
-};
-
-export type ChampionDetail = {
-  id: string;
-  name: string;
-  lore: string;
-  tags: string[];
+  blurb: string;
   info: {
     attack: number;
     defense: number;
     magic: number;
     difficulty: number;
   };
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: string;
+    y: string;
+    w: string;
+    h: string;
+  };
+  tags: string[];
+  partype: string;
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  };
+};
+
+export type ChampionDetail = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  img: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: string;
+    y: string;
+    w: string;
+    h: string;
+  };
+  skins: ChampionSkin[];
+  lore: string;
+  blurb: string;
+  allytips: string[];
+  enemytips: string[];
+  tags: string[];
+  partype: string;
+  info: {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+  };
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+  };
   spells: ChampionSpell[];
+  passive: {
+    name: string;
+    description: string;
+    img: {
+      full: string;
+      sprite: string;
+      group: string;
+      x: string;
+      y: string;
+      w: string;
+      h: string;
+    };
+  };
+};
+
+type ChampionSkin = {
+  id: string;
+  num: number;
+  name: string;
+  chromas: boolean;
 };
 
 export type ChampionSpell = {
+  id: string;
   name: string;
   description: string;
+  tooltip: string;
+  leveltip: {
+    label: string[];
+    effect: string[];
+  };
+  maxrank: number;
+  cooldown: number[];
+  cooldownBurn: string;
+  cost: number[];
+  costBurn: string;
+  range: number[];
+  rangeBurn: string;
   image: {
     full: string;
+    sprite: string;
+    group: string;
+    x: string;
+    y: string;
+    w: string;
+    h: string;
   };
+  resource: string;
 };
