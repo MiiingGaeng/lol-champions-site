6b01c2191bc45d81e0a478e7fd9a634994707269
miing_gaeng <pem4134@naver.com>
2025-03-18 11:47:45 +0900

    style: Item 페이지 스타일링 수정 #10

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,17 +1,29 @@
 import { Item } from "@/types/Items";
 import ItemCard from "@/components/ItemCard";
 import { getItemData } from "@/services/getData";
+import Image from "next/image";
 
 const itemsPage = async () => {
   const itemData = await getItemData();
   const itemList = Object.entries<Item>(itemData);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {itemList.map(([id, item]) => (
-        <ItemCard key={id} id={id} item={item} />
-      ))}
-    </div>
+    <main className="w-screen grid">
+      <div className="w-full h-[calc(100vh-64px)] fixed z-0">
+        <Image
+          src={"/images/item-background.jpg"}
+          alt="item-bg"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      <section className="w-screen grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 z-10 p-5">
+        {itemList.map(([id, item]) => (
+          <ItemCard key={id} id={id} item={item} />
+        ))}
+      </section>
+    </main>
   );
 };
 
